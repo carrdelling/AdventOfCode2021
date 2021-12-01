49e82b8265224d788cd7ca89385a2ebe39708a0e
Joaquin Derrac <carrdelling@gmail.com>
2021-12-01 08:30:46 +0000

    01Dec2021

---
+++ day1/gold.py
@@ -0,0 +1,20 @@
+
+
+def solve(depths):
+
+    return sum(1 if x < y else 0 for x, y in list(zip(depths, depths[3:])))
+
+
+def main():
+
+    with open('input_a') as in_f:
+        data = [int(row.strip()) for row in in_f]
+
+    solution = solve(data)
+
+    print(solution)
+
+
+if __name__ == "__main__":
+
+    main()
