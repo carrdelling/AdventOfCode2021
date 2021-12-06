6c306896a52732eace70d0daf02d558374ecb24f
Joaquin Derrac <carrdelling@gmail.com>
2021-12-06 07:35:43 +0000

    06Dec2021

---
+++ day6/silver.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+
+def solve(ins):
+
+    state = Counter(ins)
+
+    for _ in range(80):
+        current = sorted(state.items(), reverse=True)
+        state.clear()
+        for t, c in current:
+            if t > 0:
+                state[t-1] = c
+            else:
+                state[8] = c
+                state[6] += c
+
+    return sum(state.values())
+
+
+def main():
+
+    with open('input') as in_f:
+        data = list(map(int, in_f.readline().strip().split(',')))
+
+    solution = solve(data)
+
+    print(solution)
+
+
+if __name__ == "__main__":
+
+    main()
